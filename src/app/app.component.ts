af31053227b1d5660cc810bb41ba9b56fae2b738
IzabelSalvati <izabelsalvati@gmail.com>
2019-08-30 22:02:38 -0300

    Data Binding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+
+ peso = 70
+ altura = 1.70
+
+
+imc() : void{
+  let imc = this.peso/(this.altura*this.altura)
+      alert (imc);
+  }
 }
